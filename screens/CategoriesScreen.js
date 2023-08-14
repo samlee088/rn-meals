e08ab1f4eb9ad58853d2b75ad2e0955486e26ce0
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-13 21:10:05 -0700

    update with displaying categories

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { CATEGORIES } from '../data/dummy-data';
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+function renderCategoryItem(itemData) {
+    return <CategoryGridTile title={itemData.title} color={itemData.item.color}/>
+}
+
+
+function CategoriesScreen() {
+
+
+    return <FlatList  
+        data = { CATEGORIES } 
+        keyExtractor={(item) => item.id}
+        renderItem = {renderCategoryItem}
+        />
+}
+
+export default CategoriesScreen;
