e8ac33d68b680346333bd23cca1c86e992a97de6
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-13 21:13:51 -0700

    update with categorisScreen troubleshoot with rendering

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,7 +3,7 @@ import { FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 
 function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.title} color={itemData.item.color}/>
+    return (<CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>)
 }
 
 
